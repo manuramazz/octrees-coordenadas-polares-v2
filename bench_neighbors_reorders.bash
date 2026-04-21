d27cbff5a6ca4aef66cb97d73872aed9dc2586ad
manuramazz <manuel.ramallo@rai.usc.es>
2026-04-21 13:24:19 +0200

    bench

--- bench_neighbors_reorders.bash
+++ bench_neighbors_reorders.bash
@@ -17,36 +17,30 @@ datasets_high_density=(
 
 N_SEARCHES="5000"
 FULL_OURS="neighbors,neighborsPrune,neighborsPtr,neighborsStruct"
-FULL_ALGOS_RADIUS="neighbors,neighborsPrune,neighborsPtr,neighborsStruct,neighborsPCLKD,neighborsUnibn,neighborsPCLOct,neighborsNanoflann,neighborsPico"
+FULL_ALGOS_RADIUS="neighbors,neighborsPrune,neighborsPtr,neighborsStruct"
 THREADS="1,2,4,8,16,24,32,40"
 
 
 # subset searches
 for data in "${datasets_low_density[@]}"; do
-  ./build/octrees-benchmark -i "$data" -o "$FOLDER/subset" -r "0.5,1.0,2.0,3.0" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS"
+  ./build/octrees-benchmark -i "$data" -o "$FOLDER/subset" -r "0.5,1.0,2.0,3.0" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --local-reorder "none,spherical,cylindrical"
 done
 for data in "${datasets_high_density[@]}"; do
-  ./build/octrees-benchmark -i "$data" -o "$FOLDER/subset" -r "0.01,0.05,0.1,0.2" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS"
+  ./build/octrees-benchmark -i "$data" -o "$FOLDER/subset" -r "0.01,0.05,0.1,0.2" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --local-reorder "none,spherical,cylindrical"
 done
 
 # full searches
-./build/octrees-benchmark -i "data/semantic3d/bildstein_station1_xyz_intensity_rgb.las" -o "$FOLDER/full" -r "0.01,0.05,0.1" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-#./build/octrees-benchmark -i "data/dales_las/test/5080_54400.las" -o "$FOLDER/full" -r "5.0,10.0,15.0" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-./build/octrees-benchmark -i "data/paris_lille/Lille_0.las" -o "$FOLDER/full" -r "0.5,1.5,3.0" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-./build/octrees-benchmark -i "data/paris_lille/Paris_Luxembourg_6.las" -o "$FOLDER/full" -r "0.5,1.5,3.0" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-./build/octrees-benchmark -i "data/semantic3d/sg27_station8_intensity_rgb.las" -o "$FOLDER/full" -r "0.01,0.025,0.05" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-#./build/octrees-benchmark -i "data/speulderbos/Speulderbos_2017_TLS.las" -o "$FOLDER/full" -r "0.05,0.10,0.25" -s "all" --sequential --repeats 1  -a "neighborsPico" -e "hilb"
-./build/octrees-benchmark --kernels "cube,sphere" -i "data/semantic3d/sg27_station8_intensity_rgb.las" -o "$FOLDER/full" -r "0.01,0.02,0.03,0.05" -s "all" --sequential --repeats 1 --no-warmup -a "neighborsPtr,neighbors" -e "all"
+./build/octrees-benchmark --kernels "cube,sphere" -i "data/semantic3d/sg27_station8_intensity_rgb.las" -o "$FOLDER/full" -r "0.01,0.02,0.03,0.05" -s "all" --sequential --repeats 1 --no-warmup -a "neighborsPtr,neighbors" -e "all" --local-reorder "none,spherical,cylindrical"
 
 
 # parallel subset searches
 for data in "${datasets_low_density[@]}"; do
-    numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "$data" -o "$FOLDER/parallel_subset" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --num-threads "$THREADS" -r "0.1,0.25,0.5,1.0,2.0"
+    numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "$data" -o "$FOLDER/parallel_subset" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --num-threads "$THREADS" -r "0.1,0.25,0.5,1.0,2.0" --local-reorder "none,spherical,cylindrical"
 done
 for data in "${datasets_high_density[@]}"; do
-   numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "$data" -o "$FOLDER/parallel_subset" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --num-threads "$THREADS" -r "0.01,0.05,0.1,0.2"
+   numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "$data" -o "$FOLDER/parallel_subset" -s "$N_SEARCHES" --repeats 5 -a "$FULL_ALGOS_RADIUS" --num-threads "$THREADS" -r "0.01,0.05,0.1,0.2" --local-reorder "none,spherical,cylindrical"
 done
 
 # parallel full searches (slow)
-numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "data/paris_lille/Lille_0.las" -o "$FOLDER/parallel_full" -s "all" --sequential --repeats 1 --no-warmup -a "$FULL_ALGOS_RADIUS" --num-threads $THREADS -r "0.1,0.25,0.5,1.0,2.0"
+numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "data/paris_lille/Lille_0.las" -o "$FOLDER/parallel_full" -s "all" --sequential --repeats 1 --no-warmup -a "$FULL_ALGOS_RADIUS" --num-threads $THREADS -r "0.1,0.25,0.5,1.0,2.0" --local-reorder "none,spherical,cylindrical"
 #numactl --interleave=all ./build/octrees-benchmark --kernels "sphere" -i "data/dales_las/test/5080_54400.las" -o "$FOLDER/parallel_full" -s "all" --sequential --repeats 1 --no-warmup -a "$FULL_ALGOS_RADIUS" --num-threads $THREADS -r "0.1,0.25,0.5,1.0,2.0"
