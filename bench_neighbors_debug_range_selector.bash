001cd4e60c360c32f0236bd5cedab5633589ecfc
manuramazz <manuel.ramallo@rai.usc.es>
2026-06-08 12:14:14 +0200

    ranges to exe

--- bench_neighbors_debug_range_selector.bash
+++ bench_neighbors_debug_range_selector.bash
@@ -1,38 +1,40 @@
 # setup
-FOLDER="outv8-ranges"
+FOLDER="out_tfg_ranges"
 set -e
 mkdir -p "$FOLDER"
 
 # datasets
 datasets_low_density=(
+    "data/dales_las/5085_54320.las"
+    "data/dales_las/5095_54440.las"
+    "data/pnoa/PNOA_2024_PNR_489-4672_NPC01.las"
+)
+
+datasets_mid_density=(
+    "data/hessigheim/Mar18_train.las"
+    "data/hessigheim/Mar18_test.las"
     "data/paris_lille/Lille_0.las"
-    "data/dales_las/5145_54340.las"
     "data/paris_lille/Paris_Luxembourg_6.las"
 )
 datasets_high_density=(
     "data/semantic3d/bildstein_station1_xyz_intensity_rgb.las" 
     "data/semantic3d/sg27_station8_intensity_rgb.las"
-    # "data/pnoa/PNOA_2024_PNR_489-4672_NPC01.las"
-    #"data/speulderbos/Speulderbos_2017_TLS.las"
 )
-MAX_POINTS_LEAF=(128, 256, 512)
-UMBRALES_PODA=(32)
+
 N_SEARCHES="60"
 ALGO="neighborsPrune"
 
 # range debug searches (subsets)
 for data in "${datasets_low_density[@]}"; do
-  for leaf in "${MAX_POINTS_LEAF[@]}"; do
-    for umbral in "${UMBRALES_PODA[@]}"; do
-      ./build/octrees-benchmark -i "$data" -o "$FOLDER" -e "hilb" --kernels "cube,sphere" -r "0.1,0.3,0.5,1.0,2.0,3.0" -s "$N_SEARCHES" --repeats 1 -a "$ALGO" --local-reorder "polar,cartesian" --num-threads 1 --debug-ranges --max-leaf "$leaf" --umbral-poda "$umbral" 
-    done
-  done
+  ./build/octrees-benchmark -i "$data" -o "$FOLDER" -e "hilb" --kernels "cube,sphere" -r "1.0,2.0,3.0,5.0,6.0,7.0,8.0,10.0" -s "$N_SEARCHES" --repeats 1 -a "$ALGOS_RADIUS" --local-reorder "$LOCAL_REORDERS" --num-threads 1 --debug-ranges
+done
+
+# subset searches
+for data in "${datasets_mid_density[@]}"; do
+  ./build/octrees-benchmark -i "$data" -o "$FOLDER" -e "hilb" --kernels "cube,sphere" -r "0.25,0.5,0.75,1.0,1.5,2.0,3.0,5.0" -s "$N_SEARCHES" --repeats 1 -a "$ALGOS_RADIUS" --local-reorder "$LOCAL_REORDERS" --num-threads 1 --debug-ranges
+
 done
 
 for data in "${datasets_high_density[@]}"; do
-  for leaf in "${MAX_POINTS_LEAF[@]}"; do
-    for umbral in "${UMBRALES_PODA[@]}"; do
-      ./build/octrees-benchmark -i "$data" -o "$FOLDER" -e "hilb" --kernels "cube,sphere" -r "0.01,0.05,0.1,0.2,0.3" -s "$N_SEARCHES" --repeats 1 -a "$ALGO" --local-reorder "polar,cartesian" --num-threads 1 --debug-ranges --max-leaf "$leaf" --umbral-poda "$umbral"
-    done
-  done
+  ./build/octrees-benchmark -i "$data" -o "$FOLDER" -e "hilb" --kernels "cube,sphere" -r "0.005,0.01,0.025,0.05,0.1,0.15,0.2,0.3" -s "$N_SEARCHES" --repeats 1 -a "$ALGOS_RADIUS" --local-reorder "$LOCAL_REORDERS" --num-threads 1 --debug-ranges
 done
--- ranges_submit.sh
+++ ranges_submit.sh
@@ -4,9 +4,9 @@
 #SBATCH -p compute           # Partición (cola) de computación general
 #SBATCH -n 1                 # Un solo "task" (tu script de bash)
 #SBATCH -c 2                # Reservar 40 núcleos (coincide con tu THREADS máximo)
-#SBATCH --mem=40G            # Memoria RAM (ajusta según el peso de Semantic3D)
-#SBATCH -t 02:00:00          # Tiempo máximo (HH:MM:SS)
-#SBATCH -J debug_octree_ranges_v8    # Nombre del trabajo
+#SBATCH --mem=25G            # Memoria RAM (ajusta según el peso de Semantic3D)
+#SBATCH -t 00:10:00          # Tiempo máximo (HH:MM:SS)
+#SBATCH -J debug_octree_ranges_v9    # Nombre del trabajo
 #SBATCH -o logs/bench_%j.out # Archivo de salida (crea la carpeta logs antes)
 #SBATCH -e logs/bench_%j.err # Archivo de errores
 #SBATCH --mail-type=END,FAIL # Notificar al finalizar o si falla
@@ -16,12 +16,11 @@ module purge
 module load gcc/12.3.0
 module load papi
 
-chmod +x cesga_compile.sh
+
 chmod +x bench_neighbors_debug_range_selector.bash
 
 echo "Iniciando benchmark"
 echo "Fecha: $(date)"
 
-# bash cesga_compile.sh
 bash bench_neighbors_debug_range_selector.bash
 echo "Benchmark finalizado: $(date)"
